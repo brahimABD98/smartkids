68d8510896c2de2c68576c080bc6d4f51f72f4e4
brahimABD98 <47871510+brahimABD98@users.noreply.github.com>
2020-04-28 16:59:15 +0100

    user

--- connexion.cpp
+++ connexion.cpp
@@ -9,8 +9,8 @@ bool Connexion::ouvrirConnexion()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("Source_projet2A");
-db.setUserName("mahdiksemtini");//inserer nom de l'utilisateur
-db.setPassword("12345");//inserer mot de passe de cet utilisateur
+db.setUserName("user");//inserer nom de l'utilisateur
+db.setPassword("user");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
